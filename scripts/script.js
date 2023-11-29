1dc33d70aad3ea97841d9275033c8bc372e2b4b7
Auridebson Lima <auridebson@vutus.com>
2023-11-29 18:02:35 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,33 @@
+const especiesDeTigres = [
+    'Tigre de Bengala (Panthera tigris tigris)',
+    'Tigre da Indochina (Panthera tigris corbetti)',
+    'Tigre Malaio (Panthera tigris jacksoni)',
+    'Tigre de Amur (Panthera tigris altaica)',
+    'Tigre de Sumatra (Panthera tigris sumatrae)',
+    'Tigre do Sul da China (Panthera tigris amoyensis)',
+    'Tigre de Malayan (Panthera tigris jacksoni)',
+    'Tigre de Indonésia (Panthera tigris sondaica)',
+    'Tigre do Cáspio (Panthera tigris virgata)',
+    'Tigre da Coreia (Panthera tigris altaica)'
+  ];
+
+
 const btnFoto1 = document.querySelector("#btnFoto1")
 const btnFoto2 = document.querySelector("#btnFoto2")
 const btnFoto3 = document.querySelector("#btnFoto3")
 const btnFoto4 = document.querySelector("#btnFoto4")
 
+const card1 = document.querySelector("#card1")
+const textoCard1 = document.querySelector("#texto-card1")
 
 
 
+const listaTigres = () => {
+    textoCard1.innerHTML = ""
+    for (tigre of especiesDeTigres) {
+        textoCard1.innerHTML += `<p>${tigre}</p>`
 
-const mudaFoto = () => {
-    alert("Mudando a foto")
+    }
 }
 
-btnFoto1.addEventListener("click", mudaFoto)
+btnFoto1.addEventListener("click", listaTigres)
