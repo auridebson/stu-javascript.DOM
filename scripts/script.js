025e5174bdfbf29d611372905e2a42409e60954a
Auridebson Lima <auridebson@vutus.com>
2023-11-29 17:44:03 -0300

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const btnFoto1 = document.querySelector("#btnFoto1")
+const btnFoto2 = document.querySelector("#btnFoto2")
+const btnFoto3 = document.querySelector("#btnFoto3")
+const btnFoto4 = document.querySelector("#btnFoto4")
+
+
+
+
+
+const mudaFoto = () => {
+    alert("Mudando a foto")
+}
+
+btnFoto1.addEventListener("click", mudaFoto)
